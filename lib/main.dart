74a76b5194226b4ab97527e9e57fb757a81b0d5f
andri-mahendra <andrimahendra173@gmail.com>
2025-05-11 10:02:07 +0700

    projek barber

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'theme_controller.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
-  Get.put(ThemeController()); // INI WAJIB!
+  Get.put(ThemeController());
 
   runApp(MyApp());
 }
